6066049aa9a08ea05cb5cec428dd99c61af931f9
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-01-04 23:35:26 +0900

    Removed movie details

--- src/components/Celebrity.js
+++ src/components/Celebrity.js
@@ -52,12 +52,6 @@ class Celebrity extends Component {
       <div style={{ color: '#fff', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
         {this.state.name.length === 0 &&
           <Input onSubmit={this.onSubmit} type={inputType} title={title} />}
-        {this.state.name.length > 0 &&
-          <div style={{ width: '90%' }}>
-            <h2>Actor's Name: {this.state.name}</h2>
-            <h2>Popular Films:</h2>
-            <MovieDetails actor={this.state.name} />
-          </div>}
         {this.state.status === "loading" && <LoadWidget />}
         {this.state.status === "error" &&
           <h2>aww.. <span aria-label="poop" role="img">💩</span> there was an error!</h2>}
